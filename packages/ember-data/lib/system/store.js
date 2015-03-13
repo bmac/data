0310aa9b70dcdaab77a61c72e3cd67af702295d0
bmac <bmac325@GMail.com>
2015-03-13 14:42:12 -0400

    hasRecordForId should return false if the record is not loaded

--- packages/ember-data/lib/system/store.js
+++ packages/ember-data/lib/system/store.js
@@ -772,7 +772,7 @@ Store = Ember.Object.extend({
     var type = this.modelFor(typeName);
     var id = coerceId(inputId);
     var record = this.typeMapFor(type).idToRecord[id];
-    return !!record && !get(record, 'isEmpty');
+    return !!record && get(record, 'isLoaded');
   },
 
   /**
